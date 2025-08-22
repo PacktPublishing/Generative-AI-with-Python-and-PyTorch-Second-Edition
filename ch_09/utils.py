3424e33df026d82f0cbb124b8bcc92e29e10e645
raghavbali <bali.raghav@gmail.com>
2025-08-22 22:51:50 +0530

    Add missing utils

---
+++ ch_09/utils.py
@@ -0,0 +1,10 @@
+import torch
+
+def get_model_size(model):
+    size_model = 0
+    for param in model.parameters():
+        if param.data.is_floating_point():
+            size_model += param.numel() * torch.finfo(param.data.dtype).bits
+        else:
+            size_model += param.numel() * torch.iinfo(param.data.dtype).bits
+    return size_model
